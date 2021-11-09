febf513e8438f0ff93027196e5dc330fb7d0825e
William Shi <williamshi@college.harvard.edu>
2021-11-09 17:44:33 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CardEditor from './CardEditor';
+import CardViewer from './CardViewer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        { front: 'front1', back: 'back1' },
+        { front: 'front2', back: 'back2' },
+      ],
+      editor: true,
+    };
+  }
+
+  addCard = card => {
+    const cards = this.state.cards.slice().concat(card);
+    this.setState({ cards });
+  };
+
+  deleteCard = index => {
+    const cards = this.state.cards.slice();
+    cards.splice(index, 1);
+    this.setState({ cards });
+  };
+
+  switchMode = () => this.setState({ editor: !this.state.editor });
+
+  render() {
+    if (this.state.editor) {
+      return (
+        <CardEditor
+          addCard={this.addCard}
+          cards={this.state.cards}
+          deleteCard={this.deleteCard}
+          switchMode={this.switchMode}
+        />
+      );
+    } else {
+      return <CardViewer switchMode={this.switchMode} />;
+    }
+  }
 }
 
 export default App;
